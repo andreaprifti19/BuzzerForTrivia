bde9d4fdf5641cf77ea57d2370b8bb0db398da75
Andrea Prifti <prifti.andrea19@gmail.com>
2026-05-06 22:36:59 +0200

    ESP32 WiFi and WebSocket working, server detects winner

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,38 +1,92 @@
 #include <Arduino.h>
-#include <FastLED.h>
+#include <WiFi.h>
+#include <WebSocketsClient.h>
+#include <ArduinoJson.h>
 
-#define BUTTON_PIN 19
-#define BUZZER_PIN 18
-#define LED_PIN    4
-#define NUM_LEDS   1
+#define BUTTON_PIN  19
+#define BUZZER_PIN  18
+#define BUZZER_ID   1
 
-CRGB leds[NUM_LEDS];
+const char* SSID     = "WAVLINK-N";
+const char* PASSWORD = "EETL899003eb";
+const char* WS_HOST  = "192.168.10.168";
+const uint16_t WS_PORT = 8080;
+
+WebSocketsClient ws;
+bool wsConnected = false;
+
+void onWsEvent(WStype_t type, uint8_t* payload, size_t length) {
+  switch (type) {
+    case WStype_CONNECTED:
+      wsConnected = true;
+      Serial.println("WebSocket connected.");
+      break;
+    case WStype_DISCONNECTED:
+      wsConnected = false;
+      Serial.println("WebSocket disconnected. Will retry...");
+      break;
+    case WStype_ERROR:
+      Serial.println("WebSocket error.");
+      break;
+    default:
+      break;
+  }
+}
+
+void connectWiFi() {
+  if (WiFi.status() == WL_CONNECTED) return;
+  Serial.printf("Connecting to WiFi: %s\n", SSID);
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.printf("\nWiFi connected. IP: %s\n", WiFi.localIP().toString().c_str());
+}
 
 void setup() {
+  Serial.begin(115200);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(BUZZER_PIN, OUTPUT);
   digitalWrite(BUZZER_PIN, LOW);
 
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(64);
-  leds[0] = CRGB::Black;
-  FastLED.show();
+  connectWiFi();
+
+  ws.begin(WS_HOST, WS_PORT, "/");
+  ws.onEvent(onWsEvent);
+  ws.setReconnectInterval(3000);
 }
 
 void loop() {
-  bool pressed = digitalRead(BUTTON_PIN) == LOW;
+  // Reconnect WiFi if dropped
+  if (WiFi.status() != WL_CONNECTED) {
+    connectWiFi();
+  }
 
-  if (pressed) {
-    leds[0] = CRGB::Green;
-    FastLED.show();
+  ws.loop();
+
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    // Sound buzzer
     digitalWrite(BUZZER_PIN, HIGH);
     delay(500);
     digitalWrite(BUZZER_PIN, LOW);
-    // Wait for release before allowing another trigger
-    while (digitalRead(BUTTON_PIN) == LOW) {}
-  } else {
-    leds[0] = CRGB::Black;
-    FastLED.show();
-    digitalWrite(BUZZER_PIN, LOW);
+
+    // Send JSON message
+    if (wsConnected) {
+      StaticJsonDocument<64> doc;
+      doc["buzzer_id"] = BUZZER_ID;
+      doc["timestamp"] = millis();
+      char buf[64];
+      serializeJson(doc, buf);
+      ws.sendTXT(buf);
+      Serial.printf("Sent: %s\n", buf);
+    } else {
+      Serial.println("Button pressed but WebSocket not connected.");
+    }
+
+    // Wait for release
+    while (digitalRead(BUTTON_PIN) == LOW) {
+      ws.loop();
+    }
   }
 }
